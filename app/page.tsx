9517f03720fbb39470fae8ebfbb02037cd6fada5
satya-svt <satyavardhanthinnaluri@gmail.com>
2026-06-30 23:07:22 +0530

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { HeroSlider } from "@/components/HeroSlider";
 import { NavBar } from "@/components/NavBar";
 import { InspirationSection } from "@/components/scenes/InspirationSection";
+import VisionScene from "@/components/scenes/VisionScene";
 import { DirectorsSection } from "@/components/scenes/DirectorsSection";
+import EnquiryEngine from "@/components/scenes/EnquiryEngine";
 
 export default function Home() {
   return (
@@ -9,7 +11,13 @@ export default function Home() {
       <HeroSlider />
       <NavBar />
       <InspirationSection />
+      <div className="py-10">
+        <VisionScene />
+      </div>
       <DirectorsSection />
+      <div className="bg-white border-t border-gray-100">
+        <EnquiryEngine />
+      </div>
       
       {/* Footer logic if there's any, or we can just leave space */}
       <div className="py-20 text-center text-gray-400 text-sm">
